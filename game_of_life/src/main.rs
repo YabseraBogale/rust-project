e77c798ecbc384e8141796f3ae1bf503b20bf529
YabseraBogale <yabserabogalercd30932013@gmail.com>
2024-04-03 15:49:11 +0300

    ok i get multi

--- game_of_life/src/main.rs
+++ game_of_life/src/main.rs
@@ -1,3 +1,9 @@
 fn main() {
-    println!("Hello, world!");
+    let m:[[i32;2];2]=[[1,2],[3,4]];
+    for i in 0..2{
+        for j in 0..2{
+            println!("{0}",m[i][j]);
+        }
+    }
+    
 }
