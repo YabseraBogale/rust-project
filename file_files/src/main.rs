d32f4b34fc2beb4470ae6f89fac2f20fd88a62d8
YabseraBogale <yabserabogalercd30932013@gmail.com>
2024-06-04 19:00:21 +0300

    ok

--- file_files/src/main.rs
+++ file_files/src/main.rs
@@ -1,7 +1,8 @@
-use std::{fs::File, os::unix::process::CommandExt, process::Command};
+use std::{fs,fs::File};
 
 fn main() {
     let res=File::create_new("helloworld.txt");
     println!("{0}",res.is_err());
-
+    let res=fs::remove_file("./hello world");
+    println!("{0}",res.is_err());
 }
