32a71535ca429e7ea6cd568ca57eee06e98bb670
YabseraBogale <yabserabogalercd30932013@gmail.com>
2024-06-05 13:30:22 +0300

    ok

---
+++ multiplcation_table/target/debug/deps/multiplcation_table-a1c590d8df01cf91.d
@@ -0,0 +1,5 @@
+/home/yabsera/Documents/github/rust-project/multiplcation_table/target/debug/deps/libmultiplcation_table-a1c590d8df01cf91.rmeta: src/main.rs
+
+/home/yabsera/Documents/github/rust-project/multiplcation_table/target/debug/deps/multiplcation_table-a1c590d8df01cf91.d: src/main.rs
+
+src/main.rs:
---
+++ multiplcation_table/target/debug/deps/multiplcation_table-a836e23d14cf7a3c.d
@@ -0,0 +1,5 @@
+/home/yabsera/Documents/github/rust-project/multiplcation_table/target/debug/deps/libmultiplcation_table-a836e23d14cf7a3c.rmeta: src/main.rs
+
+/home/yabsera/Documents/github/rust-project/multiplcation_table/target/debug/deps/multiplcation_table-a836e23d14cf7a3c.d: src/main.rs
+
+src/main.rs:
