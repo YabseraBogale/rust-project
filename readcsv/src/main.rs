e9573b0edd834bf3d203929b8ae520acab351989
YabseraBogale <yabserapython@gmail.com>
2024-11-17 21:45:37 +0300

    ok

--- readcsv/src/main.rs
+++ readcsv/src/main.rs
@@ -1,3 +1,16 @@
-fn main() {
-    println!("Hello, world!");
+extern crate csv;
+use std::{error::Error, fs::File};
+fn main() -> Result<(),Box<dyn Error>>{
+    let file=File::open("SD.csv")?;
+    let mut readcsv=csv::Reader::from_reader(file);
+    let mut counter=0;
+    for data in readcsv.records(){
+        counter+=1;
+        let record=data.ok().unwrap();
+        println!("{:?}",record.iter().nth(5).unwrap().to_ascii_uppercase());
+        if counter==10{
+            break;
+        }
+    }
+    Ok(())
 }
