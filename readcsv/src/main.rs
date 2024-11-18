815fb23c96c8f71a8020612d2dfae6a07cb35e58
YabseraBogale <yabserapython@gmail.com>
2024-11-18 12:55:28 +0300

    ok

--- readcsv/src/main.rs
+++ readcsv/src/main.rs
@@ -1,5 +1,13 @@
+
 extern crate csv;
 use std::{error::Error, fs::File};
+
+#[derive(Debug)]
+struct Company{
+ name:String,
+ email:String,
+ webaddress:String,
+}
 fn main() -> Result<(),Box<dyn Error>>{
     let file=File::open("SD.csv")?;
     let mut readcsv=csv::Reader::from_reader(file);
@@ -7,7 +15,12 @@ fn main() -> Result<(),Box<dyn Error>>{
     for data in readcsv.records(){
         counter+=1;
         let record=data.ok().unwrap();
-        println!("{:?}",record.iter().nth(5).unwrap().to_ascii_uppercase());
+        let userdata=Company{
+            name:record.iter().nth(2).unwrap().to_string().replace('"',' '),
+            email:record.iter().nth(3).unwrap().to_string(),
+            webaddress:record.iter().nth(12).unwrap().to_string(),
+        };
+        println!("{:?}",userdata);
         if counter==10{
             break;
         }
